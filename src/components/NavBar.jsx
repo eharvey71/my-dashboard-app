5c7604045897e00ab6cfd27d62b3171cc0a90711
Eric Harvey <planetside0@gmail.com>
2024-06-19 16:03:52 -0400

    Add Bookmark component to NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { auth, logout } from '../services/firebaseConfig';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import Bookmark from './Bookmark';
 
 const NavBar = () => {
   const [user] = useAuthState(auth);
@@ -11,29 +12,32 @@ const NavBar = () => {
   };
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
-        <Link className="navbar-brand" to="/">Dashboard</Link>
-        <div className="collapse navbar-collapse">
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-            {user ? (
-              <li className="nav-item">
-                <button className="btn btn-outline-danger" onClick={handleLogout}>Logout</button>
-              </li>
-            ) : (
-              <>
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">Dashboard</Link>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              {user ? (
                 <li className="nav-item">
-                  <Link className="nav-link" to="/login">Login</Link>
+                  <button className="btn btn-outline-danger" onClick={handleLogout}>Logout</button>
                 </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/signup">Signup</Link>
-                </li>
-              </>
-            )}
-          </ul>
+              ) : (
+                <>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/login">Login</Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/signup">Signup</Link>
+                  </li>
+                </>
+              )}
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+      <Bookmark />
+    </div>
   );
 };
 
